3ab763af09be9350922517b464f50eed7fa7f0ee
Alex Betancourt <alex@Alexs-MacBook-Pro.local>
2022-01-26 17:53:11 -0500

    add CRUD routes to post-routes

--- models/index.js
+++ models/index.js
@@ -1,5 +1,13 @@
- // using the index.js file to collect all of the models and package them up for us.
-const Post = require('./post');
-const User = require('./User');
+// using the index.js file to collect all of the models and package them up for us.
+const Post = require("./post");
+const User = require("./User");
 
-module.exports = { User, Post};
+// create associations
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+module.exports = { User, Post };
