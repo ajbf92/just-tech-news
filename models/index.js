936ceed94ff59801b265eb49aeb039364229549e
Alex Betancourt <alex@Alexs-MacBook-Pro.local>
2022-01-26 17:28:01 -0500

    create post model and update index.js in models folder

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
  // using the index.js file to collect all of the models and package them up for us.
+const Post = require('./post');
 const User = require('./User');
 
-module.exports = { User };
+module.exports = { User, Post};
